060dc92af783722f2ab2aac54b5c96135dfb4668
akashnigam-0 <akash.20gcebai024@galgotiacollege.edu>
2024-06-09 16:53:23 +0530

    first commit

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,43 +1,8 @@
-// import React from 'react';
+import React from 'react';
 
-// const Navbar = ({ cartItemCount, onCartClick }) => {
-//   return (
-//     <nav className="navbar bg-green-900">
-//       <div className="logo">GO FOOD</div>
-//       <ul className="nav-links">
-//         <li><a href="#home">Home</a></li>
-//         <li><a href="#groceries">Groceries</a></li>
-//         <li><a href="#pages">Pages</a></li>
-//         <li><a href="#connect">Connect with Us</a></li>
-//       </ul>
-//       <div className="icons">
-//         <span className="icon">🔍</span>
-//         <span className="icon" onClick={onCartClick}>🛒 {cartItemCount}</span>
-//         <span className="icon">👤</span>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-// Navbar.js
-import React, { useState } from 'react';
-import LoginModal from './LoginModal';
-
-const Navbar = ({ onSearch, onCartClick, cartItemCount }) => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
-
-  const handleSearch = () => {
-    if (searchTerm.trim() !== '') {
-      onSearch(searchTerm.trim());
-      setSearchTerm('');
-    }
-  };
-
+const Navbar = ({ cartItemCount, onCartClick }) => {
   return (
     <nav className="navbar bg-green-900">
-      {/* Navbar contents */}
       <div className="logo">GO FOOD</div>
       <ul className="nav-links">
         <li><a href="#home">Home</a></li>
@@ -46,14 +11,10 @@ const Navbar = ({ onSearch, onCartClick, cartItemCount }) => {
         <li><a href="#connect">Connect with Us</a></li>
       </ul>
       <div className="icons">
-      <span className="icon">🔍</span>
-        <span className="icon" onClick={onCartClick}>🛒</span>
-        <span className="icon" onClick={() => setIsLoginModalOpen(true)}>👤</span>
+        <span className="icon">🔍</span>
+        <span className="icon" onClick={onCartClick}>🛒 {cartItemCount}</span>
+        <span className="icon">👤</span>
       </div>
-      {cartItemCount > 0 && <span className="cart-count">{cartItemCount}</span>}
-
-      {/* Login Modal */}
-      {isLoginModalOpen && <LoginModal onClose={() => setIsLoginModalOpen(false)} />}
     </nav>
   );
 };
